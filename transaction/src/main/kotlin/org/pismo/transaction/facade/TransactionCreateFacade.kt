fe03d7dfd7adb03a2fb32347d157034ffcc9f2b4
otavio <otavioremedio@gmail.com>
2026-01-13 15:43:25 -0300

    ajusta import

--- transaction/src/main/kotlin/org/pismo/transaction/facade/TransactionCreateFacade.kt
+++ transaction/src/main/kotlin/org/pismo/transaction/facade/TransactionCreateFacade.kt
@@ -2,19 +2,22 @@ package org.pismo.transaction.facade
 
 import org.pismo.account.service.AccountService
 import org.pismo.commons.facade.BaseFacade
+import org.pismo.transaction.context.TransactionCreateContext
 import org.pismo.transaction.dto.TransactionRequest
 import org.pismo.transaction.dto.TransactionResponse
 import org.pismo.transaction.mapper.TransactionMapper
+import org.pismo.transaction.service.OperationService
+import org.pismo.transaction.service.TransactionService
 import org.springframework.stereotype.Service
 
 @Service
 class TransactionCreateFacade(
-    private val operationService: org.pismo.transaction.service.OperationService,
+    private val operationService: OperationService,
     private val accountService: AccountService,
-    private val transactionService: org.pismo.transaction.service.TransactionService,
+    private val transactionService: TransactionService,
 ): BaseFacade() {
     fun createTransaction(request: TransactionRequest): TransactionResponse {
-        return _root_ide_package_.org.pismo.transaction.context.TransactionCreateContext(request)
+        return TransactionCreateContext(request)
             .execAndLog(::findAccount)
             .execAndLog(::findOperationType)
             .execAndLog(::createTransaction)
@@ -23,23 +26,23 @@ class TransactionCreateFacade(
             .let { it.transactionResponse!! }
     }
 
-    private fun findAccount(context: _root_ide_package_.org.pismo.transaction.context.TransactionCreateContext) =
+    private fun findAccount(context: TransactionCreateContext) =
         accountService.findById(context.request.accountId)
             .let(context::addAccount)
 
-    private fun findOperationType(context: _root_ide_package_.org.pismo.transaction.context.TransactionCreateContext) =
+    private fun findOperationType(context: TransactionCreateContext) =
         operationService.findById(context.request.operationTypeId)
             .let(context::addOperationType)
 
-    private fun createTransaction(context: _root_ide_package_.org.pismo.transaction.context.TransactionCreateContext) =
+    private fun createTransaction(context: TransactionCreateContext) =
         TransactionMapper.toEntity(context.request, context.operationType!!)
             .let(context::addTransaction)
 
-    private fun saveTransaction(context: _root_ide_package_.org.pismo.transaction.context.TransactionCreateContext) =
+    private fun saveTransaction(context: TransactionCreateContext) =
         transactionService.save(context.transaction!!)
             .let(context::addTransaction)
 
-    private fun createResponse(context: _root_ide_package_.org.pismo.transaction.context.TransactionCreateContext) =
+    private fun createResponse(context: TransactionCreateContext) =
         TransactionMapper.toResponse(context.transaction!!)
             .let(context::addTransactionResponse)
 }
